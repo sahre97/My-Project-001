433022635606f24d1b1430b4efe80bbc8c53d7c4
sahre97 <sahre.ab97@gmail.com>
2022-02-22 17:16:12 +0330

    add react app

---
+++ smple/src/firstline.js
@@ -0,0 +1,25 @@
+
+import react from 'react';
+import secondline from './secondline';
+
+function Name(){
+    return "<h2>Hi , my name is ..</h2>";
+}
+
+
+class Sample extends React.Component {
+    render() {
+      return (
+        <div>hi it is fine
+          <secondline >
+           
+          </secondline>
+    
+        </div>
+      );
+    } 
+  }
+  
+  export default Products;
+  
+  
